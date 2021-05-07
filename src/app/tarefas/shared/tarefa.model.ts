0f31990bc1df15740043444ddb28987031cc1bb5
Tiagomt <tiago-tmt@hotmail.com>
2021-05-07 13:55:23 -0300

    Criada app e serviços. Configuração dos serviços

---
+++ src/app/tarefas/shared/tarefa.model.ts
@@ -0,0 +1,10 @@
+export class Tarefa {
+
+    // interrogaçao indica atributos opcionais
+    constructor(
+        public id?: number,
+        public nome?: string,
+        public concluida?: boolean
+    ) { }
+
+}
