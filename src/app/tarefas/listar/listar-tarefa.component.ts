4c26575c71ba5d61edd5bbaffdcac690ce370606
Tiagomt <tiago-tmt@hotmail.com>
2021-06-09 16:27:42 -0300

    Configurado checkbox da tarefa

--- src/app/tarefas/listar/listar-tarefa.component.ts
+++ src/app/tarefas/listar/listar-tarefa.component.ts
@@ -36,4 +36,13 @@ export class ListarTarefaComponent implements OnInit {
 
   }
 
+  alterarStatus(tarefa: Tarefa): void {
+    if (confirm('Deseja alterar o status da tarefa "' + tarefa.nome + '"?')) {
+
+      this.tarefaService.alterarStatus(tarefa.id);
+      this.tarefas = this.listarTodos();
+    }
+
+  }
+
 }
